d5755993ff03bda1fd51c3ec7f7223a9c952e5e2
Katie Pundt <kzpundt@gmail.com>
2022-02-20 19:22:06 -0800

    add function to handle choosing a card

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -25,7 +27,10 @@ function App() {
     setTurns(0);
   }
 
-  console.log(cards, turns)
+  // handle card choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
 
   return (
     <div className="App">
@@ -33,7 +38,7 @@ function App() {
       <button onClick={ shuffleCards }>New Game</button>
       <div className="card-grid">
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,11 +1,16 @@
 import './SingleCard.css'
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice }) {
+
+  const handleClick = () => {
+    handleChoice(card);
+  }
+
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="Card front" />
-        <img className="back" src="/img/cover.png" alt="Card back" />
+        <img className="back" src="/img/cover.png" onClick={ handleClick } alt="Card back" />
       </div>
     </div>
   )
